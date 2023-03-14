72f9ad8cb9e31755bcfd0f10d71aa6b61b246b83
Cody Swendrowski <cody@ironmoosedevelopment.com>
2023-03-13 21:53:03 -0500

    Add launch command

---
+++ commands/launch.mjs
@@ -0,0 +1,90 @@
+import Config from "../config.mjs";
+import {spawn} from "child_process";
+import path from "path";
+
+export function getCommand() {
+    return {
+        command: "launch",
+        describe: "Launch Foundry VTT",
+        builder: (yargs) => {
+            yargs.option("demo", {
+                describe: "Launch Foundry VTT in demo mode",
+                type: "boolean",
+                default: false
+            });
+
+            yargs.option("port", {
+                describe: "The port to launch Foundry VTT on",
+                type: "number",
+                default: 30000
+            });
+
+            yargs.option("world", {
+                describe: "The world to launch Foundry VTT with",
+                type: "string"
+            });
+
+            yargs.option("noupnp", {
+                describe: "Disable UPnP port forwarding",
+                type: "boolean",
+                default: false
+            });
+
+            yargs.option("noupdate", {
+                describe: "Disable automatic update checking",
+                type: "boolean",
+                default: false
+            });
+
+            yargs.option("adminKey", {
+                describe: "The admin key to secure Foundry VTT's Setup screen with",
+                type: "string"
+            });
+        },
+        handler: async (argv) => {
+
+            // Run the command node main.js --debug --port 30000
+            // Launch Foundry VTT in debug mode on port 30000
+            const {demo, port, world, noupnp, noupdate, adminKey} = argv;
+
+            // Determine the installation path
+            const installPath = Config.instance.get("installPath");
+            if ( !installPath ) {
+                console.error("The installation path is not set. Use `configure set installPath <path>` to set it. Install paths look like `C:/Program Files/Foundry Virtual Tabletop`");
+                return;
+            }
+
+            // Determine the data path
+            const dataPath = Config.instance.get("dataPath");
+            if ( !dataPath ) {
+                console.error("The data path is not set. Use `configure set dataPath <path>` to set it. Data paths look like `C:/Users/Example/AppData/Local/FoundryVTT/Data`");
+                return;
+            }
+
+            function normalizePath(pathToNormalize) {
+                return path.normalize(pathToNormalize).split(path.sep).join(path.posix.sep);
+            }
+
+            // Launch Foundry VTT
+            const foundry = spawn("node", [
+                normalizePath(path.join(installPath, "resources", "app", "main.js")),
+                `--dataPath=${dataPath}`,
+                `--port=${port}`,
+                demo ? "--demo" : "",
+                world ? `--world=${world}` : "",
+                noupnp ? "--noupnp" : "",
+                noupdate ? "--noupdate" : "",
+                adminKey ? `--adminKey=${adminKey}` : ""
+            ]);
+            foundry.stdout.on("data", (data) => {
+                console.log(data.toString());
+            });
+            foundry.stderr.on("data", (data) => {
+                console.error(data.toString());
+            });
+            foundry.on("close", (code) => {
+                console.log(`Foundry VTT exited with code ${code}`);
+            });
+        }
+    }
+}
--- fvtt.mjs
+++ fvtt.mjs
@@ -4,10 +4,12 @@ import yargs from "yargs";
 import {hideBin} from "yargs/helpers";
 import {getCommand as configureCommand} from "./commands/configuration.mjs";
 import {getCommand as packageCommand} from "./commands/package.mjs";
+import {getCommand as launchCommand} from "./commands/launch.mjs";
 
 const argv = yargs(hideBin(process.argv))
     .usage("Usage: $0 <command> [options]")
     .command(configureCommand())
     .command(packageCommand())
+    .command(launchCommand())
     .help().alias('help', 'h')
     .argv;
