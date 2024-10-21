704c793e00d26214f4cb0bfdf841e7baa3cd8ae9
7H3LaughingMan <7H3LaughingMan@proton.me>
2024-10-20 19:02:40 -0500

    Update Exports

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,17 @@
-import type * as Types from "./lib/package.mjs";
-export { Types };
+import { CompileOptions } from "./lib/package.mjs";
+
+declare module "foundryvtt-cli" {
+  /**
+   * Compile source files into a compendium pack.
+   * @param {string} src   The directory containing the source files.
+   * @param {string} dest  The target compendium pack. This should be a directory for LevelDB packs, or a .db file for
+   *                       NeDB packs.
+   * @param {CompileOptions} [options]
+   * @returns {Promise<void>}
+   */
+  export function compilePack(
+    src: string,
+    dest: string,
+    options: CompileOptions
+  ): Promise<void>;
+}
