8014781691dbf712a91dccd4ea163a658634da68
Joe <182554+BoltsJ@users.noreply.github.com>
2025-05-13 12:08:52 -0500

    Add --config command line flag (#50)
    Co-authored-by: fyorl <kim.mantas@gmail.com>

--- fvtt.mjs
+++ fvtt.mjs
@@ -5,9 +5,14 @@ import { hideBin } from "yargs/helpers";
 import { getCommand as configureCommand } from "./commands/configuration.mjs";
 import { getCommand as packageCommand } from "./commands/package.mjs";
 import { getCommand as launchCommand } from "./commands/launch.mjs";
+import Config from "./config.mjs";
 
 const argv = yargs(hideBin(process.argv))
   .usage("Usage: $0 <command> [options]")
+  .config("config", "Path to YAML config file", function(configFile) {
+    Config.instance.loadLocalConf(configFile);
+    return Config.instance.getAll();
+  })
   .command(configureCommand())
   .command(packageCommand())
   .command(launchCommand())
